d522ec1e8dd7370d908195c11403d5a32a4d1fd1
Manavi Shukla <manavi139@gmail.com>
2019-04-19 13:19:39 +0530

    Update test_main.py

--- Reinforcement Learning/test_main.py
+++ Reinforcement Learning/test_main.py
@@ -3,7 +3,6 @@ from ev import vehicle
 import numpy as np
 import matplotlib.pyplot as plt 
 def main():
-    print('hello')
     #Power = vehicle()
     data = pd.read_excel (r'C:\Users\manav\Downloads\Datasets\Trip_1.xls') 
     df = pd.DataFrame(data, columns= ['Time','Speed', 'Acceleration', 'Inclination_x', 'Inclination_y', 'Power (kW)'])
@@ -11,8 +10,8 @@ def main():
     Time = d[:, 0]
     Speed = d[:, 1]
     Acceleration = d[:, 2]
-    Inclination_x = d[:, 3]
-    Inclination_y = d[:, 4]
+    Inclination_x = d[:, 3] #cos theta
+    Inclination_y = d[:, 4] #sin theta
     Pw = d[:, 5]
     Power = vehicle()
     P = Power.Power_Required(Speed, Inclination_x, Inclination_y, Acceleration)
